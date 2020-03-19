60b882479e5d57b3c26f113accd60d6c394f853b
Cris241996 <cgarciacovarrubias@gmail.com>
2020-03-19 16:48:02 -0600

    version--1

---
+++ ManualV2/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ManualV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(99, 33);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(417, 33);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(99, 104);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(263, 125);
+            this.videoSourcePlayer1.TabIndex = 2;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(244, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(244, 60);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Detener";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.videoSourcePlayer1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ManualV2/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video.DirectShow;
+using BarcodeLib.BarcodeReader;
+
+namespace ManualV2
+{
+        public partial class Form1 : Form
+        {
+            public Form1()
+            {
+                InitializeComponent();
+            }
+            /**************
+             * 
+            LEER CODIGOS QR CON WEBCAM
+             * 
+             * importar libreria barcodelib
+            **************/
+            //AGREGAR USING*/
+            //VARIABLE PARA LISTA DE DISPOSITIVOS
+            private FilterInfoCollection Dispositivos;
+            //VARIABLE PARA FUENTE DE VIDEO
+            private VideoCaptureDevice FuenteDeVideo;
+            private void Form1_Load_1(object sender, EventArgs e)
+            {
+                //LISTAR DISPOSITIVOS DE ENTRADA DE VIDEO
+                Dispositivos = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                //CARGAR TODOS LOS DISPOSITIVOS AL COMBO
+                foreach (FilterInfo x in Dispositivos)
+                {
+                    comboBox1.Items.Add(x.Name);
+                }
+                comboBox1.SelectedIndex = 0;
+            }
+            
+            private void button1_Click_1(object sender, EventArgs e)
+        {
+                //EMPEZAR A LEER
+                timer1.Enabled = true;
+
+                //ESTABLECER EL DISPOSITIVO SELECCIONADO COMO FUENTE DE VIDEO
+                FuenteDeVideo = new VideoCaptureDevice(Dispositivos[comboBox1.SelectedIndex].MonikerString);
+                //INICIALIZAR EL CONTROL
+                videoSourcePlayer1.VideoSource = FuenteDeVideo;
+                //INICIAR RECEPCION DE IMAGENES
+                videoSourcePlayer1.Start();
+            }
+
+            private void button2_Click_1(object sender, EventArgs e)
+            {
+                //DEJAR DE LEER
+                timer1.Enabled = false;
+
+
+                //DETENER RECEPCION DE IMAGENES
+                videoSourcePlayer1.SignalToStop();
+            }
+
+            private void timer1_Tick(object sender, EventArgs e)
+            {
+                //ESTAR SEGUROS QUE HAY UNA IMAGEN DESDE LA WEBCAM
+                if (videoSourcePlayer1.GetCurrentVideoFrame() != null)
+                {
+                    //IBTENER IMAGEN DE LA WEBCAM
+                    Bitmap img = new Bitmap(videoSourcePlayer1.GetCurrentVideoFrame());
+                    //UTILIZAR LA LIBRERIA Y LEER EL CÓDIGO
+                    string[] resultados = BarcodeReader.read(img, BarcodeReader.QRCODE);
+                    //QUITAR LA IMAGEN DE MEMORIA
+                    img.Dispose();
+                    //OBTENER LAS LECTURAS CUANDO SE LEA ALGO
+                    if (resultados != null && resultados.Count() > 0)
+                    {
+                        //AGREGAR EL TEXTO OBTENIDO A LA LISTA
+                        if (resultados[0].IndexOf("1111") != -1)
+                        {
+                            //QUITAR EL CODIGO DE VERIFICACION
+                            resultados[0] = resultados[0].Replace("1111", "");
+                            listBox1.Items.Add(resultados[0]);
+                        }
+                    }
+                }
+            }
+
+      
+    }
+    }
